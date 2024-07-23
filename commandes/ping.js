58cc2603359df2ab4115edb1d61fc86e1a258c6e
FredieTech <frediezra60@gmail.com>
2024-07-24 01:29:51 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -7,7 +7,7 @@ const { default: axios } = require('axios');
 zokou({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '💥', 
+    reaction: '🤔', 
     fromMe: 'true', 
 
        
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*BONIPHACE🍀 MD 💥 respond speed is*\n ```' +1000000080+ '``` *m/s*') 
+    return repondre('*DULLAH👊 MD 💥 respond speed is*\n ```' +1080+ '``` *m/s*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
