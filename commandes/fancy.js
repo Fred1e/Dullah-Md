c0d69079d38122679a5990c680fa312baf49a84f
FredieTech <frediezra60@gmail.com>
2024-07-24 01:00:15 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
 const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "🅱️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "😎" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe} POWERED BY BONIPHACE TEACH\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BONIPHACE-MD 🅱️', fancy));
+            return await repondre(`\nExemple : ${prefixe} POWERED BY DULLA\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('DULLAH MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
