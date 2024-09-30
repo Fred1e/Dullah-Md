eb6b4804c929cc7a634f1888440b6fba25c3e4f1
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-09-30 11:00:18 +0200

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/Fred1e/Dullah-Md';
-  const img = 'https://telegra.ph/file/dcaaa16f7b556d84093f7.jpg';
+  const githubRepo = 'https://api.github.com/repos/abdallahsalimjuma/Dullah-Md';
+  const img = 'https://telegra.ph/file/bb24465612098159bb5a9.jpg';
 
   try {
     const response = await fetch(githubRepo);
@@ -22,18 +22,18 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __f
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
       const gitdata = `*hellow Friend
-this is* *Dullah-md 👊.*\n *Join Group Chat* https://chat.whatsapp.com/CP2BirU5pBj04cXXgEbfuv
+this is* *Dullah-md 👊.*\n *Join Group Chat* https://chat.whatsapp.com/GufiUEQ7Jj1D2kszcZqOgx
 
 🗼 *REPOSITORY:* ${data.html_url}
 💫 *STARS:* ${repoInfo.stars}
 🧧 *FORKS:* ${repoInfo.forks}
 📅 *RELEASE DATE:* ${releaseDate}
 🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
-🙊 *OWNER:* *Mr Dulla*
+🙊 *OWNER:* *Mr Dullah*
 🍃 *THEME:* *DULLAH MD*
 🍷 *Am Safe To Fight In My Life*
 __________________________________
-            *Made With Dulla*`;
+            *Made With Dullah*`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
     } else {
