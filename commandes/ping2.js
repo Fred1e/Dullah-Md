82e9793c1594af907aa908700e27593ae6eee7ba
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-11-28 23:01:01 +0200

    Update ping2.js

--- commandes/ping2.js
+++ commandes/ping2.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping2", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*🤖Bot is online*🤖 🙏 \n\n ' + "DULLAH✌️MD  respond speed is 3000000000 m/s 😉👍";
-    let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
+    let z = '*🤖Bot is online*🤖 🙏 \n\n ' + "DULLAH✌️MD  respond speed is 30000000009 m/s 😉👍";
+    let d = '                                                                         add more status✌️';
     let varmess = z + d;
     var mp4 = 'https://files.catbox.moe/k1dhsu.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
