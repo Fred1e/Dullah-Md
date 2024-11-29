4bfb60e49c214c2de9b3f73382a8af2199c69e25
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-11-29 07:41:29 +0200

    Update and rename ping2.js to ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "ping2", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*🤖Bot is online*🤖 🙏 \n\n ' + "DULLAH✌️MD  respond speed is 30000000009 m/s ";
+    let z = '*🤖Bot is online*🤖 🙏 \n\n ' + "DULLAH✌️MD 💥 respond speed is 30000000009 m/s ";
     let d = '                                                                         add more status✌️';
     let varmess = z + d;
     var mp4 = 'https://files.catbox.moe/3qt9w3.mp4';
