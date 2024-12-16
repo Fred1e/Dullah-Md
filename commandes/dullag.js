e498826a87614117fa3b7ae9f539f89e8c32636c
abdallahsalimjuma <abdallahsalimjuma35@gmail.com>
2024-12-16 21:33:10 +0200

    Update dullag.js

--- commandes/dullag.js
+++ commandes/dullag.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*🤖Bot is online*🤖 🙏 \n\n ' + "DULLAH✌️MD ";
-    let d = '                                          add more status ';                               
+    let z = '*🤖Bot is online*🤖 🙏 \n\n ' + "DULLAH✌️MD";
+    let d = '                                          add more status';                               
     let varmess = z + d;
     var mp4 = 'https://files.catbox.moe/jq3oin.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
